1ae14498443ea8d3d10236c7449b024599316ac2
YuanChenyc <36838244+YuanChenyc@users.noreply.github.com>
2021-07-01 16:31:13 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import boto3
+import json
+
+print('Loading function')
+
+
+def handler(event, context):
+    '''Provide an event that contains the following keys:
+      - operation: one of the operations in the operations dict below
+      - tableName: required for operations that interact with DynamoDB
+      - payload: a parameter to pass to the operation being performed
+    '''
+    print("Received event: " + json.dumps(event, indent=2))
+    return "Hello,World!!"
